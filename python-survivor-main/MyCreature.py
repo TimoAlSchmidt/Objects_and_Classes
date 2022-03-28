13c3e68bf70b0ad212ffd9e27905b119dd9ae959
TimoSchmidtt <timos.minilaptop@gmail.com>
2022-03-28 10:25:37 +0200

    grabitemIfNeeded

--- python-survivor-main/MyCreature.py
+++ python-survivor-main/MyCreature.py
@@ -3,13 +3,50 @@ import random
 
 class MyCreature(core.Creature):
 
+  location: list
+
+  def __init__(self, type: str = 'Creature', name: str = 'unknown', color: str = 'white', character: str = 'x'):
+      core.Creature.__init__(self, type, name, color, character)
+      self.location = [-1,-1]
 
   def act(self, nearCreatures, nearItems):
     if len(nearCreatures) > 0 and self.attack > 0 and \
       nearCreatures[0].x == self.x and nearCreatures[0].y == self.y:
       self.hit(nearCreatures[0])
     else:
-      self.patrol(9)
+      if not self.grabItemIfNeeded(nearItems):
+        self.patrol(9)
+        
+
+  def storeCurrentLocation(self):
+    self.location[0] = self.x
+    self.location[1] = self.y
+
+
+  def grabItemIfNeeded(self, nearItems):
+    for item in nearItems:
+      if item.type is core.ItemType.WEAPON:
+        if self.attack != 3:
+          self.moveTo(item)
+          self.storeCurrentLocation()
+          return True
+        else:
+          return False
+      if item.type is core.ItemType.HEALTHPACK:
+        if self.health < self.MAX_HEALTH:
+          self.moveTo(item)
+          self.storeCurrentLocation()
+          return True
+        else:
+          return False
+      if item.type is core.ItemType.ARMOR:
+        if self.defense != 1:
+          self.moveTo(item)
+          self.storeCurrentLocation()
+          return True
+        else:
+          return False
+
 
   def moveAmountInDirection(self, amount, direction):
     if direction == 0:
@@ -33,13 +70,47 @@ class MyCreature(core.Creature):
       self.moveRight()
 
 
-  def patrol(self, distance):
+  def moveToLocation(self, place: list):
+    x0 = self._x
+    y0 = self._y
+
+    x1 = place[0]
+    y1 = place[1]
+
+    ans = True
+
+    if x0 < x1:
+      self.moveRight()
+      ans = False
+    elif x0 != x1:
+      self.moveLeft()
+      ans = False
+
+    if y0 < y1:
+      self.moveDown()
+      ans = False
+    elif y0 != y1:
+      self.moveUp()
+      ans = False
+
+    return ans
+
+
+  def patrol(self, distance=0):
     #patrouilleert strak langs de randen van de arena. (zie Wiki: arena.height etc.)
     width = arena.width-1 - distance
     height = arena.height-1 - distance
     x = self._x
     y = self._y
+    location = self.location
 
+    #if we have a valid location
+    if location[0] != -1:
+      if self.moveToLocation(location):
+        self.location = [-1, -1]
+      else:
+        return
+    
     if y != distance and y != height:
       if x != distance and x != width:
         if x > distance:
@@ -49,7 +120,6 @@ class MyCreature(core.Creature):
           self.moveRight()
           return
 
-    if y != distance and y != height:
       if y < distance:
         self.moveDown()
         return
@@ -73,13 +143,22 @@ class MyCreature(core.Creature):
 
   def __str__(self):
     return f'{self._name} {self._x, self._y}'
-
-  @property
-  def _patrolState(self):
-    return self._patrolState
+    
 
 hero = MyCreature(name=":O")
 
 arena = core.Arena(30, 30)
 arena.registerCreature(hero)
+arena.registerSource(core.Weapon, 0.0001)
+arena.registerSource(core.Weapon, 0.0001)
+arena.registerSource(core.Weapon, 0.0001)
+
+arena.registerSource(core.Armor, 0.0001)
+arena.registerSource(core.Armor, 0.0001)
+arena.registerSource(core.Armor, 0.0001)
+
+arena.registerSource(core.HealthPack, 0.0001)
+arena.registerSource(core.HealthPack, 0.0001)
+arena.registerSource(core.HealthPack, 0.0001)
+arena.pause = False
 arena.start()
