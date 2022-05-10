f73d150ae0b301ffd0a5e9a7c1e8a00a79846b5f
TimoSchmidtt <timos.minilaptop@gmail.com>
2022-05-10 09:35:37 +0200

    moveLeftTimes

--- robotarm-python-2021-main/example.py
+++ robotarm-python-2021-main/example.py
@@ -5,12 +5,17 @@ class SmartRobotArm(RobotArm):
         for i in range(times):
             super().moveRight()
 
+    def moveLeftTimes(self, times : int):
+        for i in range(times):
+            super().moveLeft()
+
 robotArm = SmartRobotArm()
 robotArm.loadMyLevel([["yellow", "yellow", "blue", "red"],[],[],[],["red"]],'moveRightTimes')
 
 # Jouw python instructies zet je vanaf hier:
 
 robotArm.moveRightTimes(4)
+robotArm.moveLeftTimes(3)
 
 
 # Na jouw code wachten tot het sluiten van de window:
